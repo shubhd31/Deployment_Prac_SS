d86b3ad3b61e762108f6fba667d35dbedc93da9d
shubhd31 <97097939+shubhd31@users.noreply.github.com>
2022-03-10 15:10:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Practical - 7 Deployment
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <h3>production-api-key</h3>
       </header>
     </div>
   );
